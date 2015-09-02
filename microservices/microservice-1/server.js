76e2506cfa010082545c8c8395695c32e7a97948
Sebastián Peyrott <speyrott@auth0.com>
2015-09-01 21:05:40 -0300

    Microservice 1 example.

---
+++ microservices/microservice-1/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var morgan = require('morgan');
+var http = require('http');
+var mongoose = require('mongoose');
+var logger = require('./logger');
+
+mongoose.connect('mongodb://localhost/test');
+
+// Our ticket sample schema
+var Ticket = mongoose.model('Ticket', {
+    id: Number,
+    status: String,
+    title: String,
+    userInitials: String,
+    assignedTo: String,
+    shortDescription: String,
+    description: String,
+    replies: [{ user: String, message: String }]
+});
+
+var app = express();
+app.use(
+    //Log requests
+    morgan(':method :url :status :response-time ms - :res[content-length]', { 
+        stream: logger.stream 
+    })
+);
+
+app.get('/tickets', function(req, res, next) {
+    Ticket.find({}, function(err, result) {
+        if(err) {
+            logger.error(err);
+            res.sendStatus(500);
+            return;
+        } 
+        res.json(result);
+    });
+});
+
+var port = process.env.PORT || 3001;
+http.createServer(app).listen(port, function (err) {
+  if (err) {
+    logger.error(err);
+  }
+  else {
+    logger.info('Listening on http://localhost:' + port);
+  }
+});
+
+
