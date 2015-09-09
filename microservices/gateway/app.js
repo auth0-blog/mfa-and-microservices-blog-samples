e7d5435bce78bf982f74168599a6f45886709c9c
Sebastián Peyrott <speyrott@auth0.com>
2015-09-09 14:18:17 -0300

    Complete gateway example (not tested).

--- microservices/gateway/app.js
+++ microservices/gateway/app.js
@@ -3,10 +3,14 @@ var url = require('url');
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 var winston = require('winston');
+require('winston-amqp');
 var morgan = require('morgan');
 var sprintf = require('sprintf');
 var Q = require('q');
 var _ = require('underscore');
+var amqp = require('amqp');
+
+var amqpHost = process.env.AMQP_HOST || 'amqp://gateway:gateway@127.0.0.1:5672';
 
 // Logging
 winston.emitErrs = true;
@@ -18,6 +22,13 @@ var logger = new winston.Logger({
             handleExceptions: true,
             json: false,
             colorize: true
+        }),
+        new winston.transports.AMQP({
+            name: 'gateway',
+            level: process.env.GATEWAY_LOG_LEVEL || 'debug',
+            host: amqpHost,
+            exchange: 'log',
+            routingKey: 'gateway'
         })
     ],
     exitOnError: false
@@ -34,6 +45,8 @@ var httpLogger = morgan('combined', { stream: logger.stream });
 function toBase64(obj) {
     return new Buffer(JSON.stringify(obj)).toString('base64');
 }
+
+var amqpConn = amqp.createConnection({url: amqpHost});
 
 var userDb = mongoose.connect(process.env.USER_DB_URL || '');
 var servicesDb = mongoose.connect(process.env.SERVICES_DB_URL || '');
@@ -67,26 +80,31 @@ function doLogin(req, res) {
     req.on('data', function(chunk) {
         try {
             var loginData = JSON.parse(chunk);
-            //FIX ME: use DB
-            var user = users[loginData.username];
-            if(user && user.password === loginData.password) {
-                var token = jwt.sign({}, secretKey, {
-                    subject: user.username,
-                    issuer: issuerStr
-                });
-                
-                res.writeHeader(200, {
-                    'Content-Length': token.length,
-                    'Content-Type': "text/plain"
-                });
-                res.write(token);
-                res.end;                
-            } else {
-                send401(res);
-            }
-        } catch(err) {
-            logger.error(err);
+            User.findOne({ username: loginData.username }, function(err, user) { 
+                if(err) {
+                    logger.error(err);
+                    send401(res);
+                    return;
+                }
             
+                if(user.password === loginData.password) {
+                    var token = jwt.sign({}, secretKey, {
+                        subject: user.username,
+                        issuer: issuerStr
+                    });
+                    
+                    res.writeHeader(200, {
+                        'Content-Length': token.length,
+                        'Content-Type': "text/plain"
+                    });
+                    res.write(token);
+                    res.end();                
+                } else {
+                    send401(res);
+                }
+            });
+        } catch(err) {
+            logger.error(err);            
             send401(res);
         }
     });
@@ -126,6 +144,73 @@ function validateAuth(data, callback) {
     }
 }
 
+function httpSend(oldReq, endpoint, data, deferred) {
+    var parsedEndpoint = url.parse(endpoint);
+
+    var options = {
+        hostname: parsedEndpoint.hostname,
+        port: parsedEndpoint.port,
+        path: parsedEndpoint.path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    var req = http.request(options, function(res) {
+        var resData = "";
+        res.on('data', function (chunk) {
+            resData += chunk;
+        });
+        res.on('end', function() {
+            deferred.resolve(resData);
+        });
+    });
+
+    req.on('error', function(e) {
+        deferred.reject({
+            req: oldReq, 
+            endpoint: endpoint, 
+            message: e.toString()
+        });
+    });
+
+    req.write(data);
+    req.end();
+}
+
+function getData(req) {
+    var result = Q.defer();
+    
+    //For simplicity
+    if(req.method !== 'POST') {
+        result.reject('Unsupported HTTP method: ' + req.method);
+        return result.promise;
+    }
+    
+    var data = "";
+    req.on('data', function(data_) {
+        data += data_;
+        if(data.length >= (1024 * 1024)) {
+            data = "";
+            result.reject("Bad request");
+        }
+    });
+    
+    req.on('end', function() {
+        if(result.promise.isPending()) {
+            try {
+                result.resolve(data);
+            } catch(err) {
+                result.reject(err.toString());
+            }
+        }
+    });
+    
+    return result.promise;
+}
+
 function httpPromise(req, endpoint) {
     var result = Q.defer();
     
@@ -137,12 +222,46 @@ function httpPromise(req, endpoint) {
         });
     }
     
-    if(req.method !== 'POST') {
-        reject('Unsupported HTTP method: ' + req.method);
-        return result.promise;
+    getData(req).then(function(data) {
+        httpSend(req, endpoint, data, result);
+    }, function(err) {
+        reject(err);
+    });
+    
+    return result.promise;
+}
+
+function amqpSend(req, endpoint, data, result) {
+    amqpConn.queue('', {
+        exclusive: true
+    }, function(queue) {
+        queue.bind('#');
+        
+        queue.subscribe({ ack: true, prefetchCount: 1 }, 
+            function(message, headers, deliveryInfo, messageObject) {
+                messageObject.acknowledge();
+                result.resolve(message);
+            }
+        );
+    });
+}
+
+function amqpPromise(req, endpoint) {
+    var result = Q.defer();
+    
+    function reject(msg) {
+        result.reject({
+            req: req, 
+            endpoint: endpoint, 
+            message: msg
+        });
     }
     
-    //TODO
+    getData(req).then(function(data) {
+        amqpSend(req, endpoint, data, result);
+    }, function(err) {
+        reject(err);
+    });
     
     return result.promise;
 }
@@ -157,7 +276,8 @@ function serviceDispatch(req, res) {
             return;
         }
         
-        //Agreggation strategy for multiple endpoints.
+        // Fanout all requests to all related endpoints. 
+        // Results are aggregated (more complex strategies are possible).
         var promises = [];
         service.endpoints.forEach(function(endpoint) {
             switch(endpoint.type) {
@@ -172,6 +292,7 @@ function serviceDispatch(req, res) {
             }
         });
         
+        //Agreggation strategy for multiple endpoints.
         Q.allSettled(promises).then(function(results) {
             var responseData = {};
         
